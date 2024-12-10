11b837c96820be03ba6d1170b71634c0c5e6a2c6
unknown <pharsh2412@gmail.com>
2024-12-10 15:46:44 +0530

    kuch toh kiya h hhh

--- src/app/api/message/route.js
+++ src/app/api/message/route.js
@@ -1,13 +1,13 @@
 import { NextResponse } from "next/server";
 
 export async function POST(request){
-    const req = await request.json()
+    
     
     // app && console.log(app)
     // sender && console.log(sender)
     // message && console.log(message)
     // group_name && console.log(group_name)
     // phone && console.log(phone)
-    console.log(req)
+    console.log(request)
     return NextResponse.json({ reply: 'User inserted successfully' });
 }
