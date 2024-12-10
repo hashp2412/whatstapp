60575d1d256302dc119cbb9802ec9e81f5abf772
unknown <pharsh2412@gmail.com>
2024-12-10 15:42:05 +0530

    kuch toh kiya h

--- src/app/api/message/route.js
+++ src/app/api/message/route.js
@@ -1,30 +1,13 @@
 import { NextResponse } from "next/server";
 
-export async function POST(request) {
-    const contentType = request.headers.get("content-type");
-
-    let body;
-    if (contentType === "application/json") {
-        // Parse as JSON
-        body = await request.json();
-    } else if (contentType === "application/x-www-form-urlencoded") {
-        // Parse as URL-encoded data
-        const text = await request.text();
-        body = Object.fromEntries(new URLSearchParams(text));
-    } else {
-        return NextResponse.json(
-            { error: "Unsupported Content-Type" },
-            { status: 415 }
-        );
-    }
-
-    const { app, sender, message, group_name, phone } = body;
-
-    app && console.log(app);
-    sender && console.log(sender);
-    message && console.log(message);
-    group_name && console.log(group_name);
-    phone && console.log(phone);
-
-    return NextResponse.json({ reply: "User inserted successfully" });
+export async function POST(request){
+    const req = await request.json()
+    
+    // app && console.log(app)
+    // sender && console.log(sender)
+    // message && console.log(message)
+    // group_name && console.log(group_name)
+    // phone && console.log(phone)
+    console.log(req)
+    return NextResponse.json({ reply: 'User inserted successfully' });
 }
