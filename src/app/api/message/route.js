715a0a6b3eb79797195ef9be099207998fc6d61c
unknown <pharsh2412@gmail.com>
2024-12-11 15:19:32 +0530

    added value extractor

--- src/app/api/message/route.js
+++ src/app/api/message/route.js
@@ -1,29 +1,17 @@
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
-    const contentType = request.headers.get("content-type");
+        const query = await request.text();
+        const params = {};
+        query.split('&').forEach(pair => {
+            if (pair) {
+                const [key, value] = pair.split('=');
+                params[key] = decodeURIComponent(value);
+            }
+        });
+        
+        console.log(params);
 
-    let body;
-
-    if (contentType.startsWith("application/x-www-form-urlencoded")) {
-        // Parse the raw body
-        const text = await request.text();
-        body = text
-    } else {
-        return NextResponse.json(
-            { error: "Unsupported Content-Type" },
-            { status: 415 }
-        );
-    }
-
-    // const { app, sender, message, group_name, phone } = body;
-
-    // app && console.log(app);
-    // sender && console.log(sender);
-    // message && console.log(message);
-    // group_name && console.log(group_name);
-    // phone && console.log(phone);
-    console.log(body)
 
     return NextResponse.json({ xyz: "User inserted successfully" });
 }
